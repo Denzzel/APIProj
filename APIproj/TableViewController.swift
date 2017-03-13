bf4083ab9f98af6703ff1e446817c70b1a853a60
Denis_Sabanov <denissabanov@mail.ru>
2017-03-14 01:50:39 +0300

    Fix adding to table

---
+++ APIproj/TableViewController.swift
@@ -0,0 +1,9 @@
+//
+//  TableViewController.swift
+//  APIproj
+//
+//  Created by Admin on 14.03.17.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+import Foundation
--- APIproj/ViewController.swift
+++ APIproj/ViewController.swift
@@ -35,6 +35,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                 //self.arrRes.adding(tmp!)
             }
             self.tableView.reloadData()
+            
         }
     }
   
